5728e0051ab71992380db19c7d88921b029ce7d8
suld2495 <suld7777@gmail.com>
2023-06-12 04:24:45 +0900

    마지막 페이지 수정

--- src/hooks/usePagination.js
+++ src/hooks/usePagination.js
@@ -1,8 +1,7 @@
 const usePagination = ({ page = 1, total = 0, perPage = 10 }) => {
-  const last = Math.ceil(total / perPage);
+  const last = Math.max(Math.ceil(total / perPage), 1);
   const start = parseInt(page / perPage) * perPage + 1;
   const end = Math.max(Math.min(start + 9, last), 1);
-
   return {
     start,
     end,

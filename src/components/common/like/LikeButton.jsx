9498560acda5544645b96bfb6331d55950483ee5
HYO00 <hyo02222@gmail.com>
2023-06-19 23:27:39 +0900

    비회원 로그인 로그인 페이지 이동 추가

--- src/components/common/like/LikeButton.jsx
+++ src/components/common/like/LikeButton.jsx
@@ -1,15 +1,20 @@
 import styled from "./like.module.css";
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import * as storeApi from "../../../services/api/store.api";
 import  Like  from "../../../assets/like.svg";
 import UnLike  from "../../../assets/unlike.svg" 
+import useAuth from "../../../hooks/useAuth";
 
 /* 좋아요 버튼을 렌더링하는 컴포넌트입니다. */
 /* storeId : 업체의 아이디 정보 */
 /* isLiked : 업체에 사용자가 좋아요 했는지 유무 */
 function LikeButton({storeId, isLiked}) {
+  const navigateTo = useNavigate();
   /* 좋아요 유무를 저장하는 상태를 생성합니다. */
   const [isLike, setIsLike] = useState(isLiked);
+   /* 유저와 관련 된 처리를 하기 위한 훅입니다. */
+  const auth = useAuth();
 
   useEffect(()=>{
     setIsLike(isLiked)
@@ -17,10 +22,15 @@ function LikeButton({storeId, isLiked}) {
 
   /* 좋아요 버튼 클릭 시 좋아요 유무를 변경하는 함수입니다. */
   const handleToggleLike = () => {
-    setIsLike((prevIsLiked) => !prevIsLiked);
+    if(auth?.user?.id){
+      setIsLike((prevIsLiked) => !prevIsLiked);
 
-    /* 변경된 내용으로 서버를 호출하는 함수입니다. */
-    updateLike()
+      /* 변경된 내용으로 서버를 호출하는 함수입니다. */
+      updateLike()
+    }else{
+      navigateTo('/login'); // 로그인 페이지로 이동
+    }
+
   };
 
   /* 좋아요 버튼 클릭 후 서버로 해당 데이터를 전송하는 함수입니다. */

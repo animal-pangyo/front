36e76c8a377d125d2ff10f4704a8f8dc6b791692
yun <ilovezltzot@naver.com>
2023-09-06 01:13:25 +0900

    [fix] 0905

--- src/components/common/settingChat/SettingChat.jsx
+++ src/components/common/settingChat/SettingChat.jsx
@@ -1,35 +1,46 @@
 import styled from "./SettingChat.module.css";
-import { useQuery, useMutation, useQueryClient } from 'react-query';
-import {fetchBlockedChatList} from "../../../services/api/chat.api";
+import { useQuery, useMutation, useQueryClient } from "react-query";
+import { fetchBlockedChatList } from "../../../services/api/chat.api";
+import { useToggleBlockMutation } from "../../../hooks/useChat";
 
 const SettingChat = () => {
-    const { data: blockedChatList, isLoading, isError } = useQuery('blockedChatList', fetchBlockedChatList);
+  const {
+    data: resData,
+    isLoading,
+    isError,
+  } = useQuery("blockedChatList", fetchBlockedChatList);
+  const blockedChatList = resData?.data.blockList;
 
-    // if (isLoading) {
-    //   return <div>Loading...</div>;
-    // }
-  
-    // if (isError) {
-    //   return <div>Error fetching blocked chat list</div>;
-    // }
-    
-    return(
-        <div className={styled.settingChatBox}>
-            <h3 className={styled.h3}>채팅 차단</h3>
-            {!blockedChatList || blockedChatList.length === 0 ? (
-                <p className={styled.p}>1:1 채팅을 차단한 멤버가 없습니다.</p>
-            ) 
-            : 
-            (
-            <ul>
-                {blockedChatList.map((chat) => (
-                    <li key={chat.id}>{chat.text}</li>
-                ))}
-            </ul>
-            )}
-        </div>
-    
-    )
+  /* 유저 차단을 시키는 함수 */
+  const toggleBlock = useToggleBlockMutation();
+
+  /* 유저 차단을 토글하는 함수입니다. */
+  const handleBlock = async () => {
+    await toggleBlock.mutateAsync();
+    alert("처리되었습니다");
+  };
+
+  return (
+    <div className={styled.settingChatBox}>
+      <h3 className={styled.h3}>채팅 차단</h3>
+      {!blockedChatList || blockedChatList.length === 0 ? (
+        <p className={styled.p}>1:1 채팅을 차단한 멤버가 없습니다.</p>
+      ) : (
+        <ul className={styled.ul}>
+          {blockedChatList.map((user) => (
+            <div className={styled.listBox}>
+              <li className={styled.li} key={user.id}>
+                {user.block_user}
+              </li>
+              <button className={styled.blockBtn} onClick={handleBlock}>
+                차단해제
+              </button>
+            </div>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default SettingChat;
--- src/services/api/chat.api.js
+++ src/services/api/chat.api.js
@@ -1,27 +1,26 @@
 import axios, { URL, getUserId } from ".";
 
-
 export const fetchChatList = () => {
-    const userId = getUserId();
-    try {
-      return axios.get(`${URL}/${userId}/chat`, {
-        headers: {
-          Accept: 'application/json', // JSON 응답을 요청합니다.
-        },
-      });; 
-    } catch (error) {
-      throw new Error('Error fetching chat list');
-    }
+  const userId = getUserId();
+  try {
+    return axios.get(`${URL}/user/chat/${userId}`, {
+      headers: {
+        Accept: "application/json", // JSON 응답을 요청합니다.
+      },
+    });
+  } catch (error) {
+    throw new Error("Error fetching chat list");
+  }
 };
 
-export  const fetchBlockedChatList = () => {
-    const userId = getUserId();
-    try {
-        return axios.get(`${URL}/block/user/${userId}`)
-      } catch (error) {
-        throw new Error('Error fetching block list');
-      }
-   
+/* 유저가 차단한 리스트를 불러오는 api */
+export const fetchBlockedChatList = () => {
+  const userId = getUserId();
+  try {
+    return axios.get(`${URL}/user/block/${userId}`);
+  } catch (error) {
+    throw new Error("Error fetching block list");
+  }
 };
 
 /* 채팅 리스트를 불러오는 api */
@@ -29,7 +28,7 @@ export const chat = (target) => {
   const userid = getUserId();
   return axios.post(`${URL}/chat`, {
     userid,
-    target
+    target,
   });
 };
 
@@ -39,21 +38,17 @@ export const checkBlock = (blockId) => {
   const id = getUserId();
   return axios.post(`${URL}/user/isBlock`, {
     id,
-    blockId
-  })
+    blockId,
+  });
 };
 
 /* 해당 유저의 차단 여부를 토글하는 api */
 /* id: 차단하려는 유저 아이디 */
-export const toggleBlock = (id) => (
-  axios.patch(`${URL}/user/block/${id}`)
-)
+export const toggleBlock = (id) => axios.patch(`${URL}/user/block/${id}`);
 
 /* 해당 유저와의 채팅룸 삭제하는 api */
 /* id: 삭제하려는 유저 아이디 */
-export const deleteChat = (id) => (
-  axios.delete(`${URL}/user/chat/${id}`)
-)
+export const deleteChat = (id) => axios.delete(`${URL}/user/chat/${id}`);
 
 /* 채팅에 이미지를 업로드할때 사용하는 api */
 /* form: 업로드 하려는 이미지 바이너리 데이터 */
@@ -61,8 +56,7 @@ export const deleteChat = (id) => (
 export const uploadFile = ({ form, chatidx }) => {
   return axios.post(`${URL}/image/chat/${chatidx}`, form, {
     headers: {
-      'Content-Type': 'multipart/form-data',
-    }
+      "Content-Type": "multipart/form-data",
+    },
   });
-}
-
+};

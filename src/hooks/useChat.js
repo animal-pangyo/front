9987f83a612149a0d2a15c5b803c60f1b6d894da
HYO00 <hyo02222@gmail.com>
2023-09-11 19:08:07 +0900

    전체 카운트 표시 쿼리 추가

--- src/hooks/useChat.js
+++ src/hooks/useChat.js
@@ -23,17 +23,22 @@ export const useChatQuery = (id) => {
 }
 
 /* 아직 읽지 않은 채팅 메시지 카운트를 가져옵니다. */
-export const useUnreadMessageCountQuery = (id) => (
-  useQuery({
-    queryKey: [ "count", id],
-    async queryFn() {
-      /* 해당 유저에 대한 차단여부를 가져오는 쿼리입니다. */
-      const response = await chatApi.getUnreadMessageCountAll(id);
-      console.log("useChat,---", response.data)
-      return response.data;
-    },
-  })
-);
+export const useUnreadMessageCountQuery = (id) => {
+  const setMsgState = useSetRecoilState(msgCntState)
+  return (
+    useQuery({
+      queryKey: [ "count", id],
+      async queryFn() {
+        /* 해당 유저에 대한 차단여부를 가져오는 쿼리입니다. */
+        const response = await chatApi.getUnreadMessageCountAll(id);
+        return response.data;
+      },
+      async onSuccess(number) {
+        setMsgState(number);
+      },
+    })
+  )
+}
   
 
 /* 해당 유저에 대한 차단여부를 가져오는 쿼리입니다. */
